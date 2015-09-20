0385aaa46fa50115eeb5b425594648c92adf3e4e
jishnuv89 <vjishnu89@gmail.com>
2015-09-21 00:33:52 +0530

    Added Usage example as comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,14 @@
 ## Functions that cache the inverse of a matrix
 ##
+## Usage example:
+##
+## > source('cachematrix.R')
+## > m <- makeCacheMatrix(matrix(c(2, 0, 0, 2), c(2, 2)))
+## > cacheSolve(m)
+## [,1] [,2]
+## [1,]  0.5  0.0
+## [2,]  0.0  0.5
+##
 ## Create a special "matrix", which is a list containing
 ## a function to
 ##   - set the value of the matrix
